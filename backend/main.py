96d69cbafd264f092e70837843779699b7b12ebd
Vladimir Ventura <vladventura@outlook.es>
2022-02-26 13:14:04 -0500

    Adding endpoint to get all the disposals near a point

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,21 @@
-import flask
-flask.Flask
+from flask import Flask, request
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/Tremont_St")
-
+@app.route("/disposals", methods=["GET"])
 def Avery_St():
-    return "AMC"
+    # Make the lat and lon required
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    dis = request.args.get('dis') if request.args.get('dis') != None else 0.5
+    if lat == None or lon == None:
+        return {'error': "Please use floats for lat and lon"}, 400
+    return {"data": [
+        {
+            'lat': 32.213,
+            'lon': -14.3213,
+            'dis_from_origin': 0.012
+        }
+    ]}, 200
 
 app.run()
