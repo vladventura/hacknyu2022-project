6c0897254255af1e19297f899dbb101b8c2a7851
Vladimir Ventura <vladventura@outlook.es>
2022-02-26 21:54:15 -0500

    Adding the ability to set userLat and userLon from the user's actual location

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,10 @@ const App = () => {
 
   const [disposals, setDisposals] = useState([]);
   const [distance, setDistance] = useState(0.5);
+  const [userLat, setUserLat] = useState(42.3513);
+  const [userLon, setUserLon] = useState(-71.0603);
 
   useEffect(() => {
-    let userLat = 4;
-    let userLon = 3;
     axios.get(`http://localhost:5000/disposals?lat=${userLat}&lon=${userLon}&dis=${distance}`)
       .then(res => {
         const { data } = res.data;
@@ -21,7 +21,7 @@ const App = () => {
     <div className="App">
       {disposals.length <= 0? <div>Nothing to see</div> 
       : 
-      <MapView />
+      <MapView userLat={userLat} userLon={userLon} />
       }
     </div>
   );
