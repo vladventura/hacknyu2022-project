ae3d796e89ffcd9422543be14a08d20813a690d4
Vladimir Ventura <vladventura@outlook.es>
2022-02-26 21:46:09 -0500

    New keyword needed

--- frontend/src/leafletIcons.js
+++ frontend/src/leafletIcons.js
@@ -1,3 +1,23 @@
 import L from 'leaflet';
 
-export const MainIcon = L.Icon({});
+export const UserIcon = new L.Icon({
+    iconUrl: require('./assets/map-marker-green.png'),
+    iconRetinaUrl: require('./assets/map-marker-green.png'),
+    iconAnchor: null,
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: [35, 35],
+    className: 'leaflet-user-icon'
+});
+
+export const DisposalIcon = new L.Icon({
+    iconUrl: require('./assets/map-marker-black.png'),
+    iconRetinaUrl: require('./assets/map-marker-black.png'),
+    iconAnchor: null,
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: [35, 35],
+    className: 'leaflet-disposal-icon'
+});
